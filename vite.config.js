2181f808c5779d221619bdc0870e56da7df2052c
KhoiNguyen <thorwaitson@gmail.com>
2022-03-19 12:55:11 -0500

    First commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  base: "./",
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "device.html"),
+      },
+    },
+  },
+});
