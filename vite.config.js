559772123eb3998e4be2bfdfb5fc6eacf264cd8b
KhoiNguyen <thorwaitson@gmail.com>
2022-03-19 18:27:00 -0500

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,16 @@ import { resolve } from "path";
 export default defineConfig({
   base: "./",
   build: {
-    manifest: true,
+    chunkSizeWarningLimit: 1900,
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "device.html"),
+        nested: resolve(__dirname, "blockcert-ui.html"),
+      },
+      output: {
+        manualChunks: {
+          "@blockcerts/cert-verifier-js": ["@blockcerts/cert-verifier-js"],
+        },
       },
     },
   },
